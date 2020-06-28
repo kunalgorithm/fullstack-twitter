c082629da83593fec7449c21819292381ef67d94
Kunal <me@kunal.sh>
2020-06-28 14:05:35 -0700

    select username and id from author on the feed

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -4,7 +4,7 @@ const prisma = new PrismaClient();
 export default async (req, res) => {
   const tweets = await prisma.tweet.findMany({
     orderBy: { createdAt: "desc" },
-    include: { author: true },
+    include: { author: { select: { username: true, id: true } } },
   });
   res.json(tweets);
 };
